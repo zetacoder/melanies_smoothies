ecc946217776c9ac4dee4818a11f8d9588b5ec69
zetacoder <amitsingh1.6180@gmail.com>
2026-02-15 05:42:20 -0600

    Update API request to use selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if(ingredient_list):
         st.subheader(fruit_chosen + ' Nutrition Information')
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)    
     #st.write(my_insert_stmt)
     #st.stop()
