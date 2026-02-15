607660809716705228dc6598e160235f682450ac
zetacoder <amitsingh1.6180@gmail.com>
2026-02-15 06:56:33 -0600

    Comment out dataframe display and stop
    Comment out dataframe display and stop commands.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 pd_df = my_dataframe.to_pandas()
 
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 ingredient_list = st.multiselect(
                   'Choose up to 5 ingredients:'
@@ -39,6 +39,8 @@ if(ingredient_list):
 
     for fruit_chosen in ingredient_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
