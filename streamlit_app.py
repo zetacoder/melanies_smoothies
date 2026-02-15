da7cf88430ae66047c972611cd39c046d5ce03ed
zetacoder <amitsingh1.6180@gmail.com>
2026-02-15 06:46:57 -0600

    Include 'SEARCH_ON' in dataframe and add st.stop()
    Updated the data selection to include 'SEARCH_ON' column and added a stop command after displaying the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,10 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect(
                   'Choose up to 5 ingredients:'
