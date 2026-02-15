da0c35ba03b3f9a8c0e9f12d1b9a4754c10a5210
zetacoder <amitsingh1.6180@gmail.com>
2026-02-15 05:30:28 -0600

    Fix response handling for Smoothiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if(ingredient_list):
 
     
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
   
     time_to_insert = st.button('Submit Order')
 
